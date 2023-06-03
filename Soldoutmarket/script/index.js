107347d58bfc52c6a3c318bc2d7da78de14c3684
Sogo13 <Backin90@yandex.ru>
2023-06-03 16:28:01 +0300

    zxc

---
+++ Soldoutmarket/script/index.js
@@ -0,0 +1,11 @@
+function changeTheme(isChecked) {
+    if (isChecked) {
+      document.body.setAttribute('dark', '');
+    } else {
+      document.body.removeAttribute('dark');
+    }
+  }
+
+  elementButton = docment.getElementById(language);
+
+  if ()
